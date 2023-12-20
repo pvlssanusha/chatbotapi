7987e6f98761428bc95e2c2d6dde3ca2e02e451d
VENKATA LAXMI SUBHA SATYA ANUSHA PERLA <87992993+pvlssanusha@users.noreply.github.com>
2023-12-20 14:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from flask import Flask, request, jsonify
 import requests
 app = Flask(__name__)
-from flask import render_template
-from pymongo import MongoClient
+
 # Dictionary to store conversation histories for different users
 user_sessions = {}
 import os
 api_key = os.getenv("GEMINI_API_KEY")
 generative_language_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + api_key
-app.config['MONGO_URI'] = os.getenv("mongo_uri")
 
-mongo = MongoClient(app.config['MONGO_URI'])
-db = mongo.get_database('Usersessions')
-collection = db.data
-print(db)
 def generate_content(conversation):
     headers = {'Content-Type': 'application/json'}
     data = {"contents": conversation}
@@ -28,20 +22,9 @@ def generate_content(conversation):
 def generate():
     user_id = request.json.get('user_id', '')
     user_input = request.json.get('user_input', '')
-    conversation_history=[]
+
     # Get or create a conversation history for the user
-    res = db.data.find_one({"userid": user_id})
-    #print("res",res)
-    if res:
-        conversation_history =res.get("history")
-        #print("ch",conversation_history)
-          # Replace 'my_collection' with your collection name
-    else:
-        datavalues={
-            "userid":user_id,
-            "history":conversation_history
-        }
-        collection.insert_one(datavalues)
+    conversation_history = user_sessions.get(user_id, [])
     user_part = {"role": "user", "parts": [{"text": user_input}]}
     conversation_history.append(user_part)
 
@@ -49,16 +32,8 @@ def generate():
 
     model_part = {"role": "model", "parts": [{"text": response_text}]}
     conversation_history.append(model_part)
-    #print(conversation_history,user_id)
-    filter_criteria = {"userid":user_id}
 
-# Specify the update operation (e.g., set a new value for a field)
-    update_operation = {"$set": {"history":conversation_history}}
+    # Update the conversation history for the user
+    user_sessions[user_id] = conversation_history
 
-# Perform the update for a single document
-    result = collection.update_one(filter_criteria, update_operation)
-    db.chat.insert_one(
-    {"userid": user_id, "history":conversation_history})
-    
-
-    return jsonify({"history":conversation_history})
+    return jsonify({"response": response_text})
